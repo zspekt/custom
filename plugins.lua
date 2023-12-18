e16ca7ae9d7f90eb0cc5a18aba5f4b151a4ea49a
speki <zspekt@tutanota.com>
2023-12-18 19:39:56 -0300

    harpoon
    Signed-off-by: speki <zspekt@tutanota.com>

--- plugins.lua
+++ plugins.lua
@@ -1,16 +1,20 @@
 local plugins = {
-  -- This should be set here in CUSTOM instead of in the .config/nvim/lua/plugins/configs/nvimtree.lua
-  -- but I am lazy. Will get around to it sometime
-  -- {
-  --   "nvim-tree/nvim-tree.lua",
-  --   options = {
-  --     view = {
-  --       number = true,
-  --       relativenumber = true
-  --     },
-  --   },
-  -- },
-  -- vimgrep arguments do NOT apply to the find_files built-in module
+
+  {
+    "ThePrimeagen/harpoon",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = true,
+    keys = {
+      { "<leader>m", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
+      { "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Go to next harpoon mark" },
+      { "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Go to previous harpoon mark" },
+      { "<leader>fi", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+	},
+
+  },
 
   {
     "olimorris/persisted.nvim",
