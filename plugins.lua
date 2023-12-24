bed1fe02fc23934ea7aa73da410bc3a21251c3cb
speki <zspekt@tutanota.com>
2023-12-24 12:52:14 -0300

    added harpoon and muren
    Signed-off-by: speki <zspekt@tutanota.com>

--- plugins.lua
+++ plugins.lua
@@ -8,12 +8,20 @@ local plugins = {
     },
     config = true,
     keys = {
-      { "<leader>m", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
-      { "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Go to next harpoon mark" },
-      { "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Go to previous harpoon mark" },
-      { "<leader>fi", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
-	},
+      { "<leader>m", "<cmd>lua require('harpoon.mark').add_file()<CR>", desc = "Mark file with harpoon" },
+      { "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<CR>", desc = "Go to next harpoon mark" },
+      { "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<CR>", desc = "Go to previous harpoon mark" },
+      { "<leader>fi", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", desc = "Show harpoon marks" },
+    },
+  },
 
+  {
+    'AckslD/muren.nvim',
+    config = true,
+    create_commands = false,
+    keys = {
+      {"<leader>rr", "<cmd>lua require('muren.api').toggle_ui()<CR>", desc = "Toggle Muren UI"},
+    },
   },
 
   {
