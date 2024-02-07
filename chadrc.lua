94daea440a4507f7fe61fb532f53a2c861d00e16
speki <zspekt@tutanota.com>
2024-02-07 15:56:22 -0300

    asd
    Signed-off-by: speki <zspekt@tutanota.com>

--- chadrc.lua
+++ chadrc.lua
@@ -1,6 +1,6 @@
----@type ChadrcConfig 
- local M = {}
- M.ui = {theme = 'catppuccin'}
- M.plugins = "custom.plugins"
- M.mappings = require "custom.mappings"
- return M
+---@type ChadrcConfig
+local M = {}
+M.ui = { theme = "kanagawa" }
+M.plugins = "custom.plugins"
+M.mappings = require "custom.mappings"
+return M
