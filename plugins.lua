5c9cc82a5cc9ac80ce61b7e751121c4a2b669eba
speki <zspekt@tutanota.com>
2024-02-22 17:08:36 -0300

    codeium binds
    Signed-off-by: speki <zspekt@tutanota.com>

--- plugins.lua
+++ plugins.lua
@@ -21,6 +21,14 @@ local plugins = {
   {
     "Exafunction/codeium.vim",
     event = "BufEnter",
+
+    config = function()
+      vim.g.codeium_no_map_tab = 1
+
+      vim.keymap.set("i", "<C-y>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true, silent = true })
+    end,
   },
 
   {
